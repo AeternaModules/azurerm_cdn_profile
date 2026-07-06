9e102cdda12566b834ee3c87be3afab9f69892c9
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:11:37 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,24 @@ output "cdn_profiles" {
   description = "All cdn_profile resources"
   value       = azurerm_cdn_profile.cdn_profiles
 }
+output "cdn_profiles_location" {
+  description = "List of location values across all cdn_profiles"
+  value       = [for k, v in azurerm_cdn_profile.cdn_profiles : v.location]
+}
+output "cdn_profiles_name" {
+  description = "List of name values across all cdn_profiles"
+  value       = [for k, v in azurerm_cdn_profile.cdn_profiles : v.name]
+}
+output "cdn_profiles_resource_group_name" {
+  description = "List of resource_group_name values across all cdn_profiles"
+  value       = [for k, v in azurerm_cdn_profile.cdn_profiles : v.resource_group_name]
+}
+output "cdn_profiles_sku" {
+  description = "List of sku values across all cdn_profiles"
+  value       = [for k, v in azurerm_cdn_profile.cdn_profiles : v.sku]
+}
+output "cdn_profiles_tags" {
+  description = "List of tags values across all cdn_profiles"
+  value       = [for k, v in azurerm_cdn_profile.cdn_profiles : v.tags]
+}
 
