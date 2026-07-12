0a4e2c8d4d0d3828a6061fbba225711a75a8982c
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:23 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "cdn_profiles" {
-  description = "All cdn_profile resources"
-  value       = azurerm_cdn_profile.cdn_profiles
-}
 output "cdn_profiles_location" {
-  description = "List of location values across all cdn_profiles"
-  value       = [for k, v in azurerm_cdn_profile.cdn_profiles : v.location]
+  description = "Map of location values across all cdn_profiles, keyed the same as var.cdn_profiles"
+  value       = { for k, v in azurerm_cdn_profile.cdn_profiles : k => v.location }
 }
 output "cdn_profiles_name" {
-  description = "List of name values across all cdn_profiles"
-  value       = [for k, v in azurerm_cdn_profile.cdn_profiles : v.name]
+  description = "Map of name values across all cdn_profiles, keyed the same as var.cdn_profiles"
+  value       = { for k, v in azurerm_cdn_profile.cdn_profiles : k => v.name }
 }
 output "cdn_profiles_resource_group_name" {
-  description = "List of resource_group_name values across all cdn_profiles"
-  value       = [for k, v in azurerm_cdn_profile.cdn_profiles : v.resource_group_name]
+  description = "Map of resource_group_name values across all cdn_profiles, keyed the same as var.cdn_profiles"
+  value       = { for k, v in azurerm_cdn_profile.cdn_profiles : k => v.resource_group_name }
 }
 output "cdn_profiles_sku" {
-  description = "List of sku values across all cdn_profiles"
-  value       = [for k, v in azurerm_cdn_profile.cdn_profiles : v.sku]
+  description = "Map of sku values across all cdn_profiles, keyed the same as var.cdn_profiles"
+  value       = { for k, v in azurerm_cdn_profile.cdn_profiles : k => v.sku }
 }
 output "cdn_profiles_tags" {
-  description = "List of tags values across all cdn_profiles"
-  value       = [for k, v in azurerm_cdn_profile.cdn_profiles : v.tags]
+  description = "Map of tags values across all cdn_profiles, keyed the same as var.cdn_profiles"
+  value       = { for k, v in azurerm_cdn_profile.cdn_profiles : k => v.tags }
 }
 
